853625a190a058cc0301b28e59a2b65178e77a47
Alessio Morale <alessiomorale@gmail.com>
2021-07-04 19:50:25 +0100

    attempt at fixing issue with publishing under https://alessiomorale.github.io/gatsby_test/

--- siteConfig.js
+++ siteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "A*Morale",
   shortName: "A*M",
   description: "a*m thoughts trash can",
-  url: "https://alessiomorale.com", // Domain of your site without prefix!,
+  url: "https://alessiomorale.github.io", // Domain of your site without prefix!,
   prefix: "/gatsby_test",
   author: "Alessio Morale",
   twitter: "alessiomorale",
